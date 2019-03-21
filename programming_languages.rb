eccf19c27b4e76dfa67952605d730952880dd38b
Joe Tustin <j.tustin@gmail.com>
2019-03-21 22:33:30 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,6 +1,9 @@
 require 'pry'
 
   # collect all the languages and types, put them in the new hash.  add the styles after
+  #basically I just drilled down to the furthest point in each value and then reconstructed it by calling out elements
+  #I had to create a new java style array to store the style elements which I could then pass in as a double attribute
+  
 def reformat_languages(languages)
   new_hash = {}
   java_style_array = Array.new
@@ -16,7 +19,7 @@ def reformat_languages(languages)
         else
           new_hash[language_name] = {type:  value, style: [style]}
         end
-        
+
       end
     end
   end
