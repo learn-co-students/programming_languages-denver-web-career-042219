f23a7deb35c5885971e4f4de807d1d468836180f
Blake A. Long <blake.a.long@gmail.com>
2019-04-09 19:53:43 -0600

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,13 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do | style, data | #style is :oo, etc; data is hash
+    data.each do | name, type | #name is ruby, etc; type is hash
+      if new_hash[name] == nil
+      new_hash[name] = type
+      new_hash[name][:style] = []
+      end 
+     new_hash[name][:style] << style
+    end 
+   end
+  new_hash
 end
